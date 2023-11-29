243adad6e559145b1becf0d7434980e3828e2e7f
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-29 11:16:50 +0100

    Finalisation projets

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # horloge
+
+Voici mon projet horloge réalisé à La Plateforme.
+# Arguments:
+1. Il s'agit d'un tuple pour regler l'heure, si le tuple est nul (0,0,0) alors il prendra l'heure local
+2. Est le format de l'horloge si am_pm est True alors le format sera sur 12h si il est False alors il le fera sur 24h
+3. L'heure de l'alarme qui devra etre rentré en 24h avec un tuple comme le premier 
+# Fonctionnalités : 
+1. Appuyer sur space pour mettre en pause l'horloge
--- console_main.py
+++ console_main.py
@@ -1,14 +1,23 @@
 import time
 import sys
 import datetime
+import keyboard
+
+# keyboard.add_hotkey("ctrl+alt+j", lambda: print("ctrl+alt+j was pressed"))
+isRunning = True
 
 def arreter_horloge():
-    pass
+    global isRunning
+    isRunning = not isRunning
+
+keyboard.add_hotkey("space", lambda: arreter_horloge())
 
 def regler_heure(tuple):
     return datetime.datetime(1,1,1, tuple[0], tuple[1], tuple[2])
     
 def afficher_heure(tuple, format, alarme):
+    global isRunning
+    isRunning = True 
     if tuple == (0,0,0):
         time_start = datetime.datetime.now()
     else:
@@ -17,8 +26,8 @@ def afficher_heure(tuple, format, alarme):
     time_alarm = regler_heure(alarme)
     
     if format:
-        while True:
-            if time_start == time_alarm:
+        while isRunning:
+            if time_start.time() == time_alarm.time():
                 print(time_start.strftime("%I:%M:%S %p"), end="\r")
                 sys.stdout.flush()
                 print("Alarm!!!!!!Alarm!")
@@ -27,9 +36,11 @@ def afficher_heure(tuple, format, alarme):
                 sys.stdout.flush()
             time_start += datetime.timedelta(seconds=1)
             time.sleep(1)
+            
+                
     else:
-        while True:
-            if time_start == time_alarm:
+        while isRunning:
+            if time_start.time() == time_alarm.time():
                 print(time_start.strftime("%H:%M:%S"), end="\r")
                 sys.stdout.flush()
                 print("Alarm!!!!!!Alarm!")
@@ -38,13 +49,22 @@ def afficher_heure(tuple, format, alarme):
                 sys.stdout.flush()
             time_start += datetime.timedelta(seconds=1)
             time.sleep(1)
+    while not isRunning:
+        continue
+    time_start = (time_start.hour,time_start.minute, time_start.second)
+    time_alarm = (time_alarm.hour,time_alarm.minute, time_alarm.second)
+    afficher_heure(time_start,format,time_alarm)
+        
+
+        
 
 am_pm = False
-afficher_heure((14,55,0), am_pm, (14,55,10))
+afficher_heure((0,0,0), am_pm, (14,50,30))
 
 #Si le tuple est nul alors il nous affichera l'heure locale
 # Pour régler am/pm ou 24h nous utilisons un booléen
 # True pour am/pm; False pour le format 24h
 # Le troisième parametre configure l'heure de l'alarme
+# Avec al touche space vous pouvez stopper l'horloge
 
 # L'alarme ne sonne pas lorsqu'on utilise l'argument tuple=(0,0,0)
